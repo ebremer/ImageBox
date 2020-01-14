c98341c7ce2311cb5f95640578468c010af6f2cb
Erich Bremer <erich@ebremer.com>
2020-01-14 13:42:21 -0500

    improve iiif support

--- src/main/java/com/ebremer/imagebox/NeoTiler.java
+++ src/main/java/com/ebremer/imagebox/NeoTiler.java
@@ -3,11 +3,14 @@ package com.ebremer.imagebox;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.StringWriter;
 import static java.lang.Math.abs;
 import java.util.Hashtable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObjectBuilder;
 import loci.common.DebugTools;
 import loci.common.Location;
 import loci.common.services.DependencyException;
@@ -23,18 +26,12 @@ import loci.formats.meta.MetadataStore;
 import loci.formats.services.OMEXMLService;
 import ome.xml.meta.OMEXMLMetadataRoot;
 import ome.xml.model.primitives.PositiveInteger;
-import org.apache.jena.query.DatasetFactory;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.riot.JsonLDWriteContext;
-import org.apache.jena.riot.RDFFormat;
-import org.apache.jena.riot.RDFWriter;
 
 /**
  *
  * @author erich
  */
-public class NeoTiler {
+public final class NeoTiler {
     private SVSReader reader;
     private ServiceFactory factory;
     private OMEXMLService service;
@@ -55,9 +52,12 @@ public class NeoTiler {
     private boolean borked = false;
     private String status = "";
     private long lastaccessed;
+    private CoreMetadata big;
+    private String url;
     
     public NeoTiler(String f) {
         DebugTools.enableLogging("ERROR");
+        url = f;
         lastaccessed = System.nanoTime();
         String getthis;
         if (f.startsWith("http")) {
@@ -108,11 +108,10 @@ public class NeoTiler {
             py = new int[numi];
             pr = new int[numi];
             pi = new int[numi];
-            CoreMetadata big;
             //for (int j=0;j<reader.getSeriesCount();j++) {
-              //  big = reader.getCoreMetadataList().get(j);
-                //System.out.println(j+" >>> "+big.sizeX+","+big.sizeY+" aspect ratio : "+(((float) big.sizeX)/((float)big.sizeY)));
-            //}
+//                big = reader.getCoreMetadataList().get(j);
+//                System.out.println(j+" >>> "+big.sizeX+","+big.sizeY+" aspect ratio : "+(((float) big.sizeX)/((float)big.sizeY)));
+//            }
             big = reader.getCoreMetadataList().get(lowerbound);
             float ratio = ((float) big.sizeX)/((float) big.sizeY);
             for (int j=lowerbound;j<(numi+lowerbound);j++) {
@@ -208,7 +207,7 @@ public class NeoTiler {
             }
         }
     }
-    
+/*    
     public String GetImageInfo() {
         Model m = ModelFactory.createDefaultModel();
         if (borked) {
@@ -218,7 +217,14 @@ public class NeoTiler {
             m.addLiteral(m.createResource("http://www.ebremer.com/a"), m.createProperty("http://www.w3.org/2003/12/exif/ns#width"), iWidth);
             m.addLiteral(m.createResource("http://www.ebremer.com/a"), m.createProperty("http://www.w3.org/2003/12/exif/ns#xResolution"), Math.round(10000/mppx));
             m.addLiteral(m.createResource("http://www.ebremer.com/a"), m.createProperty("http://www.w3.org/2003/12/exif/ns#yResolution"), Math.round(10000/mppy));
-            m.addLiteral(m.createResource("http://www.ebremer.com/a"), m.createProperty("http://www.w3.org/2003/12/exif/ns#resolutionUnit"), 3);   
+            m.addLiteral(m.createResource("http://www.ebremer.com/a"), m.createProperty("http://www.w3.org/2003/12/exif/ns#resolutionUnit"), 3);
+
+            for (int j=0;j<px.length;j++) {
+                System.out.println(j+" >>> "+px[j]+","+py[j]+" aspect ratio : "+pr[j]);
+                m.add(m.createResource("http://www.ebremer.com/a"), m.createProperty("http://iiif.io/api/image/2#sizes"), m.createResource("http://www.ebremer.com/"+j));
+                m.addLiteral(m.createResource(BlankNodeId.create("dummy-"+j).toString()), m.createProperty("http://www.w3.org/2003/12/exif/ns#height"), py[j]);
+                m.addLiteral(m.createResource(BlankNodeId.create("dummy-"+j).toString()), m.createProperty("http://www.w3.org/2003/12/exif/ns#width"), px[j]);
+            }
         }
         StringWriter out = new StringWriter();
         JsonLDWriteContext ctx = new JsonLDWriteContext();
@@ -232,6 +238,59 @@ public class NeoTiler {
             .build();
         w.output(out);
         return out.toString();
+    }
+*/
+    public String GetImageInfo() {
+        JsonBuilderFactory jbf = Json.createBuilderFactory(null);
+        JsonObjectBuilder value = jbf.createObjectBuilder()
+                .add("@id", "http://localhost:8888/iiif/?iiif=/TCGA-B6-A1KC-01Z-00-DX1.4DD3E48B-F434-499F-9FF1-0FFD2883A375.svs")
+                .add("@context", "http://iiif.io/api/image/2/context.json")
+                .add("height", iHeight)
+                .add("width", iWidth)
+                .add("xResolution", Math.round(10000/mppx))
+                .add("yResolution", Math.round(10000/mppx))
+                .add("resolutionUnit", 3);
+        
+        JsonArrayBuilder sizes = jbf.createArrayBuilder();
+        JsonArrayBuilder scalefactors = jbf.createArrayBuilder();
+        for (int j=0;j<px.length-3;j++) {
+            System.out.println(j+" >>> "+px[j]+","+py[j]+" aspect ratio : "+pr[j]);
+            scalefactors.add(pr[j]);
+            sizes.add(jbf.createObjectBuilder().add("width", px[j]).add("height", py[j]));
+        }
+        value.add("sizes", sizes);
+        JsonArrayBuilder profile = jbf.createArrayBuilder();
+        JsonArrayBuilder supports = jbf.createArrayBuilder();
+        supports
+                .add("canonicalLinkHeader")
+                .add("profileLinkHeader")
+                .add("mirroring")
+                .add("rotationArbitrary")
+                .add("sizeAboveFull")
+                .add("regionSquare");
+        JsonArrayBuilder qualities = jbf.createArrayBuilder();
+        qualities
+                .add("default")
+                .add("bitonal")
+                .add("gray")
+                .add("color");
+        JsonArrayBuilder formats = jbf.createArrayBuilder();
+        formats
+                .add("jpg")
+                .add("png")
+                .add("gray")
+                .add("color");
+        profile.add("http://iiif.io/api/image/2/level2.json")
+               .add(jbf.createObjectBuilder()
+                        .add("supports", supports)
+                        .add("formats", formats)
+                        .add("qualities", qualities));
+        JsonArrayBuilder tiles = jbf.createArrayBuilder().add(jbf.createObjectBuilder()
+                                                                .add("width", reader.getOptimalTileWidth())
+                                                                .add("height", reader.getOptimalTileHeight())
+                                                                .add("scaleFactors", scalefactors));
+        value.add("protocol","http://iiif.io/api/image").add("profile", profile).add("tiles", tiles);
+        return value.build().toString();
     }
     
     public BufferedImage FetchImage(int x, int y, int w, int h, int tx, int ty) {
