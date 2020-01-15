93e47c3ebdcc777cf2e8723817728e808727b297
Erich Bremer <erich@ebremer.com>
2020-01-14 22:16:43 -0500

    fix Image Reader Pool cleanup

--- src/main/java/com/ebremer/imagebox/ImageReaderPool.java
+++ src/main/java/com/ebremer/imagebox/ImageReaderPool.java
@@ -1,27 +1,38 @@
 package com.ebremer.imagebox;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  *
  * @author erich
  */
 public class ImageReaderPool {
-    private final HashMap pool = new HashMap();
+    private final ConcurrentHashMap<String, IRL> pool;
     private final Timer timer = new Timer();
-    private final File f = new File("cache");
+    //private final File f = new File("cache");
     
     ImageReaderPool() {
+        this.pool = new ConcurrentHashMap<>();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                //System.out.println("purging old objects...");
+                Iterator<String> i = pool.keySet().iterator();
+                while (i.hasNext()) {
+                    String tag = i.next();
+                    IRL irl = pool.get(tag);
+                    long age = ((System.nanoTime()-irl.getLastAccess())/1000000000);
+                    if (age>Settings.MaxAgeReaderPool) {
+                        //System.out.println("purging "+tag);
+                        RemovePool(tag);
+                    }
+                    //System.out.println(tag + "  "+ age);
+                }
             }
-        }, 60*60*1000, 60*60*1000);
+        }, Settings.ReaderPoolScanDelay, Settings.ReaderPoolScanRate);
     }
     
     private synchronized NeoTiler GetReaderFromPool(String id) {
@@ -29,13 +40,13 @@ public class ImageReaderPool {
         NeoTiler reader;
         if (pool.containsKey(id)) {
             //System.out.println("Found Reader in pool! : "+id);
-            ArrayList list = (ArrayList) pool.get(id);                    
-            reader = (NeoTiler) list.remove(0);
+            ArrayList<IRO> list = pool.get(id).getPool();
+            IRO iro = list.remove(0);
+            reader = iro.getNeoTiler();
             if (list.isEmpty())  {
                 pool.remove(id);
             }
         } else {
-            //System.out.println("creating new neotiler : "+id);
             reader = new NeoTiler(id);
         }
         return reader;
@@ -49,18 +60,22 @@ public class ImageReaderPool {
         }
         return reader;
     }
-    
+
+    public synchronized void RemovePool(String id) {
+        pool.remove(id);
+    }
+
     public synchronized void ReturnReader(String id, NeoTiler reader) {
         //System.out.println("ReturnReader "+id);
         if (pool.containsKey(id)) {
-            ArrayList list = (ArrayList) pool.get(id);            
-            list.add(reader);
-            //System.out.println("pool size [e]: "+list.size());
+            ArrayList<IRO> list = pool.get(id).getPool();
+            list.add(new IRO(reader));
+          //  System.out.println("pool size [e]: "+list.size());
         } else {
             //System.out.println("creating new list...");
-            ArrayList list = new ArrayList();
-            list.add(reader);
-            pool.put(id, list);
+            ArrayList<IRO> list = new ArrayList<>();
+            list.add(new IRO(reader));
+            pool.put(id, new IRL(list));
             //System.out.println("pool size [ne]: "+list.size());
         }        
     }
--- src/main/java/com/ebremer/imagebox/Settings.java
+++ src/main/java/com/ebremer/imagebox/Settings.java
@@ -7,4 +7,7 @@ package com.ebremer.imagebox;
 public class Settings {
     static int port = 8888;
     static String webfiles = "/segdemo";
+    static long MaxAgeReaderPool = 600;
+    static long ReaderPoolScanDelay = 600;
+    static long ReaderPoolScanRate = 60;
 }
